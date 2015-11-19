f613db7aecf610ef88a582f817e74f6c4ff895c5
mdunser <marc.dunser@cern.ch>
2015-11-19 16:46:18 +0100

    post-preapproval update of the code. this is now almost a good version still have to implement stuff in the resultproducer though

--- synchExercise.py
+++ synchExercise.py
@@ -41,9 +41,12 @@ if __name__ == '__main__':
 
     print 'Going to load DATA and MC trees...'
     #synchDatasets = ['TTLep_pow']
-    synchDatasets = ['DoubleMuon_Run2015C_25ns_05Oct_v1_runs_246908_258714' , 'DoubleEG_Run2015C_25ns_05Oct_v1_runs_246908_258714' , 'MuonEG_Run2015C_25ns_05Oct_v1_runs_246908_258714' ,
-                     'DoubleMuon_Run2015D_05Oct_v1_runs_246908_258751'      , 'DoubleEG_Run2015D_05Oct_v1_runs_246908_258751'      , 'MuonEG_Run2015D_05Oct_v2_runs_246908_258751'      ,
-                     'DoubleMuon_Run2015D_v4_runs_246908_258751'            , 'DoubleEG_Run2015D_v4_runs_246908_258751'            , 'MuonEG_Run2015D_v4_runs_246908_258751'            ]
+    ## 1p3 fb-1 synchDatasets = ['DoubleMuon_Run2015C_25ns_05Oct_v1_runs_246908_258714' , 'DoubleEG_Run2015C_25ns_05Oct_v1_runs_246908_258714' , 'MuonEG_Run2015C_25ns_05Oct_v1_runs_246908_258714' ,
+    ## 1p3 fb-1                  'DoubleMuon_Run2015D_05Oct_v1_runs_246908_258751'      , 'DoubleEG_Run2015D_05Oct_v1_runs_246908_258751'      , 'MuonEG_Run2015D_05Oct_v2_runs_246908_258751'      ,
+    ## 1p3 fb-1                  'DoubleMuon_Run2015D_v4_runs_246908_258751'            , 'DoubleEG_Run2015D_v4_runs_246908_258751'            , 'MuonEG_Run2015D_v4_runs_246908_258751'            ]
+    synchDatasets = ['DoubleMuon_Run2015C_25ns-05Oct_v1_runs_246908_260627' , 'DoubleEG_Run2015C_25ns-05Oct_v1_runs_246908_260627' , 'MuonEG_Run2015C_25ns-05Oct_v1_runs_246908_260627' ,
+                     'DoubleMuon_Run2015D-05Oct_v1_runs_246908_260627'      , 'DoubleEG_Run2015D-05Oct_v1_runs_246908_260627'      , 'MuonEG_Run2015D-05Oct_v2_runs_246908_260627'      ,
+                     'DoubleMuon_Run2015D_v4_runs_246908_260627'            , 'DoubleEG_Run2015D_v4_runs_246908_260627'            , 'MuonEG_Run2015D_v4_runs_246908_260627'            ]
     treeSynch = Sample.Tree(helper.selectSamples(inputFileName, synchDatasets, 'SYNCH'), 'SYNCH'  , 1)
 
     print 'Trees successfully loaded...'
@@ -58,19 +61,19 @@ if __name__ == '__main__':
         actualTree = treeSynch.blocks[0].samples[ind].ttree
 
         ## define the variables you want to have scanned
-        scanString  = "run:lumi:evt"
+        scanString  = "run:lumi:evt:met_pt:Jet_pt[0]:Jet_pt[1]:Jet_pt[2]:nJetSel_Edge:nBJetMedium35_Edge"
         #scanString += "Lep1_pt_Edge:Lep1_eta_Edge:Lep1_phi_Edge:Lep1_pdgId_Edge:Lep1_miniRelIso_Edge:Lep1_mvaIdSpring15_Edge:t.Lep1_minTauDR_Edge:"
         #scanString += "Lep2_pt_Edge:Lep2_eta_Edge:Lep2_phi_Edge:Lep2_pdgId_Edge:Lep2_miniRelIso_Edge:Lep2_mvaIdSpring15_Edge:t.Lep2_minTauDR_Edge:"
         #scanString += "lepsMll_Edge:met_pt:met_rawPt:nJetSel_Edge:nBJetMedium35_Edge:rhoCN:(Lep1_pdgId_Edge*Lep2_pdgId_Edge)"
 
         ## define the cut you want to have applied
         cutString = cuts.goodLepton
-        cutString = cuts.AddList([cuts.METJetsSignalRegion, cuts.GoodLeptonSF(), cuts.Central(), cuts.Zmass ])
+        cutString = cuts.AddList([cuts.METJetsSignalRegion, cuts.GoodLeptonSF(), cuts.Zmass ])
         #print cutString
 
         actualTree.SetScanField(-1)
         save = os.dup( sys.stdout.fileno() )
-        newout = file( 'evtLists/vinceSynch_%d.txt'%(ind), 'w' )
+        newout = file( 'evtLists/synchOnFullDataset_%d.txt'%(ind), 'w' )
         os.dup2( newout.fileno(), sys.stdout.fileno() )
         actualTree.Scan(scanString, cutString, 'colsize=12')#colsize=8 precision=8 col=8d:12d');
         os.dup2( save, sys.stdout.fileno() )
