65aa119dbfd03dc19481b477729f282f86182712
mdunser <marc.dunser@cern.ch>
2016-06-24 10:04:44 +0200

    new helper function needed for ewino

--- include/helper.py
+++ include/helper.py
@@ -1,6 +1,127 @@
-import math, sys, os
+import math, sys, os, copy
 import ROOT as r
 from   ROOT import TGraphErrors, gROOT, TCanvas, TFile
+
+def ratioError(num, num_e, den, den_e, opt=''):
+    tmp_n = r.TH1F('n', 'n', 1,0,1)
+    tmp_d = r.TH1F('d', 'd', 1,0,1)
+    tmp_r = r.TH1F('r', 'r', 1,0,1)
+    tmp_n.SetBinContent(1,num); tmp_n.SetBinError(1,num_e)
+    tmp_d.SetBinContent(1,den); tmp_d.SetBinError(1,den_e)
+    #tmp_n.Divide(tmp_d)
+    tmp_r.Divide(tmp_n, tmp_d, 1., 1., opt)
+    return tmp_r.GetBinContent(1), tmp_r.GetBinError(1)
+
+def makeRatioPlot(hlist, name, opts = {}):
+
+    norm = False; do = 'pe'; legco = [0.6,0.5,0.88,0.9];
+    if opts.has_key('norm' ): norm = opts['norm']
+    if opts.has_key('do'   ): do   = opts['do']
+    if opts.has_key('legco'): legco= opts['legco']
+
+    c1 = r.TCanvas(); c1.cd()
+    pad1 = r.TPad("pad1", "pad1", 0, 0.25, 1, 1.0)
+    pad1.SetBottomMargin(0.1)
+    pad1.Draw()
+    pad2 = r.TPad("pad2", "pad2", 0, 0.05, 1, 0.25)
+    pad2.SetTopMargin(0.1);
+    pad2.SetBottomMargin(0.3);
+    pad2.Draw();
+
+    leg = r.TLegend(legco[0], legco[1], legco[2], legco[3])
+    ymax = 0.
+    for ih,h in enumerate(hlist):
+        tmp_max = h.GetMaximum()
+        if tmp_max > ymax: ymax = tmp_max
+
+    pad1.cd()
+    for ih,h in enumerate(hlist):
+        if norm: h.Scale(1./h.Integral())
+        else: h.GetYaxis().SetRangeUser(0.01, ymax*1.2)
+        h.Draw(do + ('' if not ih else ' same'))
+        #leg.AddEntry(h, ' '.join(h.GetName().replace('_norm','').replace('_',' ').split()[:-1]), 'pl')
+        leg.AddEntry(h, ' '.join(h.GetName().replace('_norm','').replace('_',' ')), 'pl')
+
+    leg.Draw('same')
+
+    if opts.has_key('fitratio'): 
+        fitratio = opts['fitratio']
+        print 'found ratio fit key. fitting index %d with function %s'%(fitratio[0], fitratio[1])
+    else: fitratio = [-1 , ''] 
+    r_fiti = fitratio[0]; r_fitf = fitratio[1]
+    ratios = []
+    pad2.cd()
+    for ih,h in enumerate(hlist):
+        if not ih: 
+            refHisto = copy.deepcopy(h)
+        else:
+            divh = copy.deepcopy(h)
+            divh.Divide(refHisto)
+            divh.GetYaxis().SetRangeUser(0.0,2.0)
+            divh.GetYaxis().SetTitle('ratio')
+            divh.GetYaxis().CenterTitle()
+            divh.GetYaxis().SetTitleSize(0.14)
+            divh.GetYaxis().SetLabelSize(0.12)
+            divh.GetYaxis().SetNdivisions(4)
+            divh.GetXaxis().SetLabelSize(0.12)
+            divh.GetXaxis().SetTitle('')
+            if ih == r_fiti:
+                divh.Fit(r_fitf,'','',50,250)
+                retval = copy.deepcopy(divh.GetFunction(fitratio[1]))
+            #divh.Draw('pe same')#+ '' if ih == 1 else ' same')
+            ratios.append(divh)
+
+    for ratio in ratios:
+        ratio.Draw('e0 same')
+
+    l1 = r.TLine(divh.GetXaxis().GetXmin(), 1., divh.GetXaxis().GetXmax(), 1.)
+    l1.SetLineStyle(2)
+    l1.Draw('same')
+
+    c1.SaveAs(name+'.png')
+    c1.SaveAs(name+'.pdf')
+    c1.SaveAs(name+'.root')
+    pad1.cd()
+    pad1.SetLogy(1)
+    leg.SetX1NDC(0.35); leg.SetY1NDC(0.25); leg.SetX2NDC(0.58); leg.SetY2NDC(0.55)
+    c1.SaveAs(name+'_log.png')
+    c1.SaveAs(name+'_log.pdf')
+    c1.SaveAs(name+'_log.root')
+
+    if r_fiti >= 0:
+        return retval
+
+class vParams:
+    def __init__(self,name):
+        self.name = name
+        self.setParams()
+    def setParams(self):
+        if   self.name == 'met': self.xmin =   0   ; self.xmax = 300 ; self.nbins = 12; self.ymax = 0.35; self.title = 'ME_{T} (GeV)'         ; self.vtree = 'met_Edge'            ;
+        elif self.name == 'mt2': self.xmin =   0   ; self.xmax = 200 ; self.nbins = 25; self.ymax = 0.35; self.title = 'MT_{2} (GeV)'         ; self.vtree = 'mt2_Edge'            ;
+        elif self.name == 'hjj': self.xmin =  50   ; self.xmax = 300 ; self.nbins = 25; self.ymax = 0.35; self.title = 'm_{jj, hard}'         ; self.vtree = 'hardMjj_Edge'        ;
+        elif self.name == 'ldr': self.xmin = 0.1   ; self.xmax = 0.5 ; self.nbins =  4; self.ymax = 0.08; self.title = '#Delta R_{ll}'        ; self.vtree = 'lepsDR_Edge'         ;
+        elif self.name == 'mll': self.xmin = 11.   ; self.xmax = 121.; self.nbins = 22; self.ymax = 0.08; self.title = 'm_{ll}'               ; self.vtree = 'lepsMll_Edge'        ;
+        elif self.name == 'ldphi': self.xmin = 0.  ; self.xmax = 3.15; self.nbins = 25; self.ymax = 0.08; self.title = '#Delta #phi_{ll}'     ; self.vtree = 'lepsDPhi_Edge'       ;
+        elif self.name == 'zpt': self.xmin = 0.0   ; self.xmax = 250 ; self.nbins = 25; self.ymax = 0.24; self.title = 'p_{T}^{ll} (GeV)'     ; self.vtree = 'lepsZPt_Edge'        ;
+        elif self.name == 'ht': self.xmin = 0.0    ; self.xmax = 250 ; self.nbins = 25; self.ymax = 0.24; self.title = 'H_{T} (GeV)'          ; self.vtree = 'htJet35j_Edge'       ;
+        elif self.name == 'mlb': self.xmin = 0.0   ; self.xmax = 500 ; self.nbins = 25; self.ymax = 0.30; self.title = '#Sigma m_{lb} (Gev)'  ; self.vtree = 'sum_mlb_Edge'        ;
+        elif self.name == 'a3d': self.xmin = 0.0   ; self.xmax = 3.15; self.nbins = 25; self.ymax = 0.14; self.title = '#Delta 3D'            ; self.vtree = 'd3D_Edge'            ;
+        elif self.name == 'jjp': self.xmin = 0.0   ; self.xmax = 3.15; self.nbins = 25; self.ymax = 0.14; self.title = '#Delta #phi_{jj,hard}'; self.vtree = 'abs(hardJJDphi_Edge)';
+        elif self.name == 'j1mphi': self.xmin = 0.0; self.xmax = 3.15; self.nbins = 25; self.ymax = 0.14; self.title = '#Delta #phi_{j1,met}' ; self.vtree = 'abs(j1MetDPhi_Edge)' ;
+        elif self.name == 'j2mphi': self.xmin = 0.0; self.xmax = 3.15; self.nbins = 25; self.ymax = 0.14; self.title = '#Delta #phi_{j2,met}' ; self.vtree = 'abs(j2MetDPhi_Edge)' ;
+        elif self.name == 'jjdr': self.xmin = 0.0  ; self.xmax = 5.00; self.nbins = 25; self.ymax = 0.14; self.title = '#Delta R_{j1,j2}'     ; self.vtree = 'abs(hardJJDR_Edge)'  ;
+        elif self.name == 'l1pt': self.xmin = 20.  ; self.xmax = 270.; self.nbins = 25; self.ymax = 0.14; self.title = 'p_{T}^{l1}'           ; self.vtree = 'Lep1_pt_Edge'        ;
+        elif self.name == 'l2pt': self.xmin = 20.  ; self.xmax = 270.; self.nbins = 25; self.ymax = 0.14; self.title = 'p_{T}^{l2}'           ; self.vtree = 'Lep2_pt_Edge'        ;
+        elif self.name == 'l1eta': self.xmin = -3. ; self.xmax = 3.  ; self.nbins = 25; self.ymax = 0.14; self.title = '#eta^{l1}'            ; self.vtree = 'Lep1_eta_Edge'       ;
+        elif self.name == 'l2eta': self.xmin = -3. ; self.xmax = 3.  ; self.nbins = 25; self.ymax = 0.14; self.title = '#eta^{l2}'            ; self.vtree = 'Lep2_eta_Edge'       ;
+        elif self.name == 'nj': self.xmin =  0.    ; self.xmax = 3.  ; self.nbins =  3; self.ymax = 0.14; self.title = 'n_{jets}'             ; self.vtree = 'nJetSel_Edge'        ;
+        elif self.name == 'nb': self.xmin =  0.    ; self.xmax = 3.  ; self.nbins =  3; self.ymax = 0.14; self.title = 'n_{b-jets}'           ; self.vtree = 'nBJetMedium35_Edge'  ;
+        elif self.name == 'metBIN': self.xmin = 100; self.xmax = 600 ; self.nbins = 10; self.ymax = 0.35; self.title = 'ME_{T} (GeV)'         ; self.vtree = 'met_Edge'            ;
+        elif self.name == 'nll': 
+            self.xmin = 12  ; self.xmax =  30  ; self.nbins =  6 ; self.ymax = 0.20 ; self.title = 'NLL'                 ;
+            self.vtree = '-1.*TMath::Log(lh_ana_met_data_Edge*lh_ana_mlb_data_Edge*lh_ana_a3d_data_Edge*lh_ana_zpt_data_Edge)'; 
+        else:
+            sys.exit('TRYING TO CALL VPARAMS WITH UNKNOWN VARIABLE')
 
 def ensureDirectory(_path):
    #d = os.path.dirname(_path)
@@ -47,6 +168,16 @@ class color:
        bold = '\033[1m'
        underline = '\033[4m'
        end = '\033[0m'
+
+class bcolors: 
+    HEADER = '\033[95m' 
+    OKBLUE = '\033[94m' 
+    OKGREEN = '\033[92m' 
+    WARNING = '\033[93m' 
+    FAIL = '\033[91m' 
+    ENDC = '\033[0m' 
+    BOLD = '\033[1m' 
+    UNDERLINE = '\033[4m' 
 
 class valErrs:
     def __init__(self, cen_val, cen_sys, cen_stat, fwd_val, fwd_sys, fwd_stat, name):
