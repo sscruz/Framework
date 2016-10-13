15a626036deadc474d6a9d108b30f0ab9a5e0be6
sscruz <sscruz@gmail.com>
2016-10-13 10:14:40 +0200

    yield tables (MC) for different mll regions

---
+++ yieldEstimation.py
@@ -0,0 +1,54 @@
+import ROOT as r
+from   ROOT import gROOT, TCanvas, TFile, TF1, TH1F, TGraph
+import math, sys, optparse, copy, re, array, itertools, os
+
+
+import include.helper     as helper
+import include.Region     as Region
+import include.Canvas     as Canvas
+import include.CutManager as CutManager
+import include.Sample     as Sample
+import include.Tables     as Tables
+
+
+
+
+if __name__ == "__main__":
+   
+
+    gROOT.SetBatch(True)
+    gROOT.ProcessLine('.L include/tdrstyle.C')
+    r.setTDRStyle() 
+    r.gStyle.SetPaintTextFormat("4.2f")
+    
+    
+    global lumi
+    lumi= 35
+    mcDatasets = ['TT_pow_ext34']
+    dyDatasets = ['DYJetsToLL_M10to50','DYJetsToLL_M50_HT100to200_ext','DYJetsToLL_M50_HT200to400_ext',
+                  'DYJetsToLL_M50_HT400to600_ext','DYJetsToLL_M50_HT600toInf']
+
+    treeTT = Sample.Tree(helper.selectSamples('samples.dat', mcDatasets, 'MC'), 'MC', 0, isScan = 0)
+    treeDY = Sample.Tree(helper.selectSamples('samples.dat', dyDatasets, 'MC'), 'MC', 0, isScan = 0)
+
+    cuts = CutManager.CutManager()
+    mllCut = {'low': 'lepsMll_Edge < 81',
+              'onz': 'lepsMll_Edge > 81 && lepsMll_Edge < 101',
+              'high1': 'lepsMll_Edge > 101 && lepsMll_Edge < 200',
+              'high2': 'lepsMll_Edge > 200 && lepsMll_Edge < 300',
+              'high3': 'lepsMll_Edge > 300'}
+    Cuts = [cuts.SignalRegionBaseLineNoTrigger, cuts.SF]
+    print 'yields for %4.1f /fb'%lumi
+    for process in ['TT','DY']:
+        tree = treeTT if 'TT' in process else treeDY
+        print 'Process', process
+        print '\t ttbar-like \t  Non-ttbar-like'
+        for mll in ['low','onz','high1','high2','high3']:
+            yields = []
+            for nll in ['ttbar','non-ttbar']:
+                nllCut = 'nll_Edge > 21' if 'non' in nll else 'nll_Edge < 21' 
+                theCuts = Cuts + [nllCut] + [mllCut[mll]]
+                yields.append(tree.getYields(lumi,'0.5',0,1,cuts.AddList(theCuts)))
+            print '%s\t %4.1f +/- %4.1f\t %4.1f +/- %4.1f'%(mll,
+                                                            yields[0][0], yields[0][1],
+                                                            yields[1][0], yields[1][1])
